4453d2b8d44864516d5d48977be3707fde7d259b
kesha11S <124372416+kesha11S@users.noreply.github.com>
2024-04-25 01:57:16 +0900

    Lighting, terrain, controller support

---
+++ Assets/InternalAssets/Scripts/MeshSimplify.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class MeshSimplify : MonoBehaviour
+{
+    //public float quality = 0.3f;
+    
+    /*
+    void Start()
+    {
+        //NOT A GOOD PRACTICE
+        //https://stackoverflow.com/questions/32989808/adding-a-meshfilter-mesh-via-script
+        MeshFilter meshfilter = gameObject.AddComponent<MeshFilter>();
+        GameObject go = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        meshfilter.mesh = go.GetComponent<MeshFilter>().mesh;
+        meshfilter.gameObject.AddComponent<MeshRenderer>();
+        Destroy(go);
+    }    
+    
+    void OnGUI()
+    {
+           if (GUI.Button(Rect.MinMaxRect(0, 0, 200, 200),"Simplify Mesh"))
+           {
+                var originalMesh = GetComponent<MeshFilter>().sharedMesh;
+                var meshSimplifier = new UnityMeshSimplifier.MeshSimplifier();
+                meshSimplifier.Initialize(originalMesh);
+                meshSimplifier.SimplifyMesh(quality);
+                var destMesh = meshSimplifier.ToMesh();
+                GetComponent<MeshFilter>().sharedMesh = destMesh;
+           }
+    }
+    */
+}
--- Assets/InternalAssets/Scripts/TeleportationAnchor.cs
+++ Assets/InternalAssets/Scripts/TeleportationAnchor.cs
@@ -1,15 +1,28 @@
+//Скрипт телепортации, при нажатии на точку телепортации
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TeleportationAnchor : MonoBehaviour
 {
+    //Проверяет, нажал ли пользователь на эту точку
+    public void OnPointerClick() 
+    {
+        //Перемещает пользователя (GameObject с тегом "Игрок") на позицию 
+        //точки телепортации плюс высота пользователя
+        GameObject.FindWithTag("Player").transform.position = transform.position + new Vector3(0, 1.6f, 0);
+        
+        //Вывод в коман
+        Debug.Log("You teleported :)");
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         
     }
+
+    
 
     public void OnPointerEnter()
     {
@@ -20,16 +33,6 @@ public class TeleportationAnchor : MonoBehaviour
     {
         
     }
-
-
-    public void OnPointerClick()
-    {
-        GameObject.FindWithTag("Player").transform.position = transform.position + new Vector3(0, 1.6f, 0);
-        Debug.Log("You teleported :)");
-    }
-
-
-
 
     // Update is called once per frame
     //void Update()
