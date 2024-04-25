331c8c46496bfe2cdcf67ed93091aa24d2b44aba
kesha11S <124372416+kesha11S@users.noreply.github.com>
2024-04-26 00:33:06 +0900

    added tuesok

--- Assets/InternalAssets/Scripts/MeshSimplify.cs
+++ Assets/InternalAssets/Scripts/MeshSimplify.cs
@@ -6,18 +6,21 @@ using UnityEngine.Serialization;
 
 public class MeshSimplify : MonoBehaviour
 {
-    //public float quality = 0.3f;
-    
     /*
+    public float quality = 0.5f;
+    
+    
     void Start()
     {
         //NOT A GOOD PRACTICE
         //https://stackoverflow.com/questions/32989808/adding-a-meshfilter-mesh-via-script
+        
         MeshFilter meshfilter = gameObject.AddComponent<MeshFilter>();
         GameObject go = GameObject.CreatePrimitive(PrimitiveType.Capsule);
         meshfilter.mesh = go.GetComponent<MeshFilter>().mesh;
         meshfilter.gameObject.AddComponent<MeshRenderer>();
         Destroy(go);
+        
     }    
     
     void OnGUI()
@@ -33,4 +36,5 @@ public class MeshSimplify : MonoBehaviour
            }
     }
     */
+    
 }
