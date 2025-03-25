aa906d2cd5f50a55c0756200075ff0be2828edc9
kesha11S <124372416+kesha11S@users.noreply.github.com>
2025-03-26 02:55:11 +0900

    Kerdugen. Sounds

--- Assets/InternalAssets/Scripts/RotateObject.cs
+++ Assets/InternalAssets/Scripts/RotateObject.cs
@@ -8,11 +8,20 @@ public class RotateObject : MonoBehaviour
     public Material GazedMaterial;
     public Material UngazedMaterial;
     public GameObject rotatingObject;
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip audioClip;
 
     //Проверяет, нажал ли пользователь на эту точку
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void OnPointerClick() 
     {
         rotatingObject.transform.Rotate(0.0f, 15.0f, 0.0f, Space.World);
+        audioSource.clip = audioClip;
+        audioSource.Play();
     }
 
     public void OnPointerEnter()
--- Assets/InternalAssets/Scripts/TeleportationAnchor.cs
+++ Assets/InternalAssets/Scripts/TeleportationAnchor.cs
@@ -7,16 +7,24 @@ public class TeleportationAnchor : MonoBehaviour
 {
     public Material GazedMaterial;
     public Material UngazedMaterial;
-  
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip audioClip;
 
 
     //Проверяет, нажал ли пользователь на эту точку
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void OnPointerClick() 
     {
         //Перемещает пользователя (GameObject с тегом "Игрок") на позицию 
         //точки телепортации плюс высота пользователя
         GameObject.FindWithTag("Player").transform.position = transform.parent.transform.position + new Vector3(0, 1.6f, 0);
-        
+        audioSource.clip = audioClip;
+        audioSource.Play();
+
         //Выводит дебаг сообщение, что пользователь телепортировался
         Debug.Log("You teleported :)");
     }
